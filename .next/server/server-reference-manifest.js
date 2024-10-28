598674b2167820fbb7786ee666794bbc8ccce5e4
RukshanPokharel <rukshanpokharel@gmail.com>
2024-10-28 11:58:20 +0100

    merge master to main
    merge master to main

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6k6voi3yUcpxzfXmJ9bZfwHnuohX3Q6pHJRIYdSIqrI=\"\n}"
---
+++ .next/static/chunks/app_page_tsx_c154bb._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_c154bb._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_c154bb._.js",
+  "chunks": [
+    "static/chunks/_a3a68e._.js"
+  ],
+  "source": "dynamic"
+});
